4b9173909dd17288d7595b975834ff6e28abec26
Emanuela <emlicame@student.codam.nl>
2023-02-04 18:52:39 +0100

    removed last attempt to reduce activities after last meal

--- src/routine.c
+++ src/routine.c
@@ -6,7 +6,7 @@
 /*   By: emlicame <emlicame@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 15:50:08 by emlicame          #+#    #+#             */
-/*   Updated: 2023/02/04 18:06:20 by emlicame         ###   ########.fr       */
+/*   Updated: 2023/02/04 18:50:16 by emlicame         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,6 @@ int	philo_eating(t_philo *philo)
 	philo->meals_eaten++;
 	pthread_mutex_unlock(&philo->data->mutex_meals);
 	ph_mutex_unlock_fork(philo);
-	if (!check_life(philo->data))
-		return (1);
 	return (0);
 }
 
@@ -62,12 +60,9 @@ void	*ph_routine(void *philo_stru)
 		usleep(500);
 	while (check_life(philo->data))
 	{
-		if (philo_eating(philo))
-			return (NULL);
-		if (philo_sleeping(philo))
-			return (NULL);
-		if (philo_thinking(philo))
-			return (NULL);
+		philo_eating(philo);
+		philo_sleeping(philo);
+		philo_thinking(philo);
 	}
 	return (NULL);
 }
